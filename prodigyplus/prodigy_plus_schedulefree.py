865c76dc298e99c585d27abda9e65e125be686f3
Logan <loganbooker@gmail.com>
2024-11-18 21:46:15 +1100

    Fix eps when None

--- prodigyplus/prodigy_plus_schedulefree.py
+++ prodigyplus/prodigy_plus_schedulefree.py
@@ -116,7 +116,7 @@ class ProdigyPlusScheduleFree(torch.optim.Optimizer):
             raise ValueError("Invalid d0 value: {}".format(d0))
         if not 0.0 < lr:
             raise ValueError("Invalid learning rate: {}".format(lr))
-        if not 0.0 < eps:
+        if eps is not None and not 0.0 < eps:
             raise ValueError("Invalid epsilon value: {}".format(eps))
         if not 0.0 <= betas[0] < 1.0:
             raise ValueError("Invalid beta parameter at index 0: {}".format(betas[0]))
