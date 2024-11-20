75af74fefb1bfc57d372f967b78541f95f561ff8
LoganBooker <loganbooker@gmail.com>
2024-11-21 10:00:58 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'prodigy-plus-schedule-free',
   packages = find_packages(exclude=[]),
-  version = '1.2.0',
+  version = '1.3.0',
   license='Apache 2.0',
   description = 'Prodigy + ScheduleFree',
   author = 'Logan Booker',
