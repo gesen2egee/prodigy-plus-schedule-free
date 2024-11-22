Fixed precision issue with bfloat16 and new optimised factorisation

prodigyplus/prodigy_plus_schedulefree.py
@@ -413,12 +413,14 @@ class ProdigyPlusScheduleFree(torch.optim.Optimizer):
             if isinstance(exp_avg_sq, list):
                 row_var, col_var, dr, dc, reduce_dc = exp_avg_sq
 
-                grad_sqr = grad.square().add_(1e-30)
-                row_var.mul_(beta2).add_(grad_sqr.mean(dim=dr, keepdim=True), alpha=one_minus_beta2_d)
-                col_var.mul_(beta2).add_(grad_sqr.mean(dim=dc, keepdim=True), alpha=one_minus_beta2_d)
-                del grad_sqr
-
-                row_col_mean = row_var.mean(dim=reduce_dc, keepdim=True)
+                row_var.mul_(beta2).add_(
+                    grad.norm(dim=dr, keepdim=True).square_().div_(grad.shape[dr]), 
+                alpha=one_minus_beta2_d)
+                col_var.mul_(beta2).add_(
+                    grad.norm(dim=dc, keepdim=True).square_().div_(grad.shape[dc]), 
+                alpha=one_minus_beta2_d)
+                
+                row_col_mean = row_var.mean(dim=reduce_dc, keepdim=True).add_(1e-30)
                 row_factor = row_var.div(row_col_mean).sqrt_()
                 col_factor = col_var.sqrt()
                 denom = row_factor * col_factor