667b26fbfe19006ebf4d2fdaeb8b228c5b243ed9
Logan <loganbooker@gmail.com>
2024-11-23 17:12:05 +1100

    Update version 1.4.0 -> 1.4.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 setup(
   name = 'prodigy-plus-schedule-free',
   packages = find_packages(exclude=[]),
-  version = '1.4.0',
+  version = '1.4.1',
   license='Apache 2.0',
   description = 'Automatic learning rate optimiser based on Prodigy and Schedule-Free',
   author = 'Logan Booker',
